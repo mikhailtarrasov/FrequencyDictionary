c31bf3fb7bf412eb9af42992a048c7045cf72cd4
Михаил <mikhailtarrasov@gmail.com>
2017-07-29 22:51:42 +0500

    Работа с файлом и добавлен класс для составления словаря

---
+++ FrequencyDictionary/FrequencyDictionary/CountDictionary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FrequencyDictionary
+{
+    public class CountDictionary
+    {
+        private Dictionary<string, int> Dictionary { get; set; }
+
+        public CountDictionary(IEnumerable<string> words)
+        {
+            Dictionary = words.GroupBy(x => x).ToDictionary(pair => pair.Key, pair => pair.Count());
+        }
+
+        public Dictionary<string, int> GetDictionary()
+        {
+            return Dictionary;
+        }
+    }
+}
---
+++ FrequencyDictionary/FrequencyDictionary/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FrequencyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0];
+            var fileText = String.Empty;
+
+            try
+            {   
+                using (var sr = new StreamReader(path, Encoding.Default))
+                {
+                    fileText = sr.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Файл не может быть прочитан. Ошибка:");
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+                return;    
+            }
+
+            var outputPath = path + ".output.txt";
+
+            var textUnifier = new TextUnifier(fileText);
+            var words = textUnifier.GetStringWords();
+
+            var dictionary = new CountDictionary(words);    
+            var sortedDict = dictionary.GetDictionary().OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            using (var sw = new StreamWriter(outputPath))
+            {
+                foreach (var var in sortedDict)
+                {
+                    sw.WriteLine(var.Key + "\t" + var.Value);
+                }
+            }
+        }
+    }
+}
